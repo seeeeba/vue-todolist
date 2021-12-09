64c5a185db68990a0bd4df42408119a4b0ec3ccd
seeeeba <lillo.sebastian1@gmail.com>
2021-12-09 18:55:15 +0100

    scaffolding milestone 3

---
+++ Milestone-3/js/script.js
@@ -0,0 +1,32 @@
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+const app = new Vue(
+    {
+        el :'#root',
+        data: {
+            todoArray: [
+                {
+                    testo: 'Fare la spesa',
+                    done: true,
+                },
+                {
+                    testo: 'Fare gli esercizi di Boolean',
+                    done: false,
+                },
+                {
+                    testo: 'Aprire un ticket',
+                    done: false,
+                },
+                {
+                    testo: 'Finire entro le 19 per andare a correre',
+                    done: false,
+                }
+            ],
+        },
+        methods:{
+            removeLi : function (index){
+                this.todoArray.splice(index, 1);
+            }
+        }
+    }
+)
